4b154cd96e9b5e7b081e8688bdf34f57d3fd9972
tanish goyal <tanishgoyal16@gmail.com>
2024-10-26 22:16:45 +0530

    Module 10 Lesson 1

---
+++ Module 10/Printer/src/Homework.jsx
@@ -0,0 +1,14 @@
+const Homework = () => {
+  return (
+    <div>
+      <h1> Shopping Cart </h1>
+      <ul>
+        <li> Bread </li>
+        <li> Coke </li>
+        <li> Chocolate </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Homework;
