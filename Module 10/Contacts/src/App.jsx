28a640ff31ef55ce9712be6a0e63c01d8533ecab
tanish goyal <tanishgoyal16@gmail.com>
2024-11-16 23:13:57 +0530

    homework

--- Module 10/Contacts/src/App.jsx
+++ Module 10/Contacts/src/App.jsx
@@ -6,21 +6,29 @@ const App = () => {
     <>
       <h1 className="title"> My Contacts project </h1>
       <div className="contacts">
-        <Contact name="Name" phoneNo="Phone Number" email="Email"></Contact>
+        <Contact
+          name="Name"
+          phoneNo="Phone Number"
+          email="Email"
+          dob="DOB"
+        ></Contact>
         <Contact
           name="Tanish"
           phoneNo="1010101010"
           email="tan@gmail.com"
+          dob="28/20/08"
         ></Contact>
         <Contact
           name="Lucy"
           phoneNo="2020202020"
           email="lucy@gmail.com"
+          dob="28/20/08"
         ></Contact>
         <Contact
           name="Nadia"
           phoneNo="9922992299"
           email="nad29@gmail.com"
+          dob="28/20/08"
         ></Contact>
       </div>
     </>
--- Module 10/Contacts/src/Contact.jsx
+++ Module 10/Contacts/src/Contact.jsx
@@ -1,12 +1,13 @@
 import "./Contact.css";
 
 const Contact = (props) => {
-  const { name, phoneNo, email } = { ...props };
+  const { name, phoneNo, email, dob } = { ...props };
   return (
     <div className="contact">
       <h2 className="name"> {name} </h2>
       <p className="phone"> {phoneNo} </p>
       <p className="email"> {email} </p>
+      <p className="dob"> {dob} </p>
     </div>
   );
 };
